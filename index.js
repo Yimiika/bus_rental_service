2412a33dd69da77104adfb5f1ce03c4a4c2f8958
Adebayo Otomuola <114394143+OtomuolaAdebayo@users.noreply.github.com>
2025-05-13 11:52:05 -0700

    Add new cors url

--- index.js
+++ index.js
@@ -41,12 +41,13 @@ app.use(methodOverride("_method"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    // origin: "http://localhost:3000",
+    origin: "https://valueride.vercel.app",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/", authRoute);
